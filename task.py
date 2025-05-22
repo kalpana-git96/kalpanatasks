f23f2777bbe5e3ea621a6767fa016db1ec88d64d
kalpana-git96 <ky971141@gmail.com>
2025-05-22 16:45:16 +0530

    Create task.py

---
+++ task.py
@@ -0,0 +1,11 @@
+#Task 1: Perform Basic Mathematical Operations
+
+a=input('Enter the first number: ')
+b=input('Enter the first number: ')
+a=int(a)
+b=int(b)
+print('Addition :', a+b)
+print('Subtraction:',a-b)
+print('Multiplication:',a*b)
+print('Division:',a/b)
+
